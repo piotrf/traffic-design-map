30ad789ee12789583a03f774d483738768bac6ef
Piotr <piotr@piotrf.pl>
2017-07-12 23:15:54 +0200

    Setup simple Map component tree.

---
+++ src/react/Map/Map.jsx
@@ -0,0 +1,19 @@
+// import PropTypes from 'prop-types';
+import React from 'react';
+
+import Playground from './Playground.jsx';
+
+require('./Map.css');
+
+const Map = props => (
+  <div className="td-map">
+    Hello Map
+    <Playground />
+  </div>
+);
+
+// Map.propTypes = {};
+
+// Map.defaultProps = {};
+
+export default Map;
---
+++ src/react/Map/Playground.jsx
@@ -0,0 +1,10 @@
+// import PropTypes from 'prop-types';
+import React from 'react';
+
+const Playground = props => <div />;
+
+// Playground.propTypes = {};
+
+// Playground.defaultProps = {};
+
+export default Playground;
