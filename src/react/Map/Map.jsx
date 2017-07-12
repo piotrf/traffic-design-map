6ee555e704b3e0ece4f017c0a6fbb52ee89dad27
Piotr <piotr@piotrf.pl>
2017-07-12 23:51:40 +0200

    Cleanup.

--- src/react/Map/Map.jsx
+++ src/react/Map/Map.jsx
@@ -1,19 +1,23 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
+import GoogleMap from 'google-map-react';
 
-import Playground from './Playground.jsx';
-
-require('./Map.css');
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const Map = props => (
-  <div className="td-map">
-    Hello Map
-    <Playground />
-  </div>
+  <GoogleMap bootstrapURLKeys={{ key: 'AIzaSyApjk_AdVABnyN0PWIjr52lx5Gg_GZRMgc' }} defaultCenter={props.center} defaultZoom={props.zoom}>
+    <AnyReactComponent lat={54.5189} lng={18.5305} text={'Gdynia'} />
+  </GoogleMap>
 );
 
-// Map.propTypes = {};
+Map.propTypes = {
+  // center: PropTypes.obj
+  zoom: PropTypes.number,
+};
 
-// Map.defaultProps = {};
+Map.defaultProps = {
+  center: { lat: 54.5189, lng: 18.5305 },
+  zoom: 14,
+};
 
 export default Map;
