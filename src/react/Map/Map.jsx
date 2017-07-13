f629f36925d5621af6bc5bfbb4bf159b3a8014d8
Piotr <piotr@piotrf.pl>
2017-07-13 18:53:09 +0200

    Add min/max zoom options.

--- src/react/Map/Map.jsx
+++ src/react/Map/Map.jsx
@@ -7,11 +7,11 @@ import ClusterMarker from '../Markers/ClusterMarker.jsx';
 
 const MarkerHOC = ({ title, text }) => <SimpleMarker title={title} text={text} />;
 
-const Map = props => {
-  const markers = props.mapData.markers;
-  const markersIds = props.mapData.markersIds;
+const Map = ({ mapData, options }) => {
+  const markers = mapData.markers;
+  const markersIds = mapData.markersIds;
   return (
-    <GoogleMap bootstrapURLKeys={{ key: 'AIzaSyApjk_AdVABnyN0PWIjr52lx5Gg_GZRMgc' }} defaultCenter={props.mapData.center} defaultZoom={props.mapData.zoom}>
+    <GoogleMap bootstrapURLKeys={{ key: 'AIzaSyApjk_AdVABnyN0PWIjr52lx5Gg_GZRMgc' }} defaultCenter={mapData.center} defaultZoom={mapData.zoom} options={options}>
       {markersIds.map(markerId => {
         const marker = markers[markerId];
         return <MarkerHOC key={marker.id} lat={marker.lat} lng={marker.lng} title={marker.title} text={marker.text} />;
@@ -22,8 +22,14 @@ const Map = props => {
 
 Map.propTypes = {
   mapData: PropTypes.object.isRequired,
+  options: PropTypes.object,
 };
 
-Map.defaultProps = {};
+Map.defaultProps = {
+  options: {
+    minZoom: 6,
+    maxZoom: 20,
+  },
+};
 
 export default Map;
